339865abc04e46da36e8917926336e8833c3e287
machen <machen27@gmail.com>
2017-09-28 14:58:19 -0400

    Add second figure in multimeter analysis
    First figure is all data, second figure is zoomed in on frist 1.5 hours.

--- Data/ResultsAnalysis.py
+++ Data/ResultsAnalysis.py
@@ -3,6 +3,7 @@ import re
 import pandas as pd
 import matplotlib.pyplot as plt
 import seaborn as sns
+import itertools as iT
 
 """ Script is designed to be run on the csvs here
 user should move files out
@@ -28,21 +29,33 @@ if len(workingFiles) == 0:
 # Initialize figures
 f1 = plt.figure(1)
 ax1 = f1.add_subplot(111)
+f2 = plt.figure(2)
+ax2 = f2.add_subplot(111)
 plt.ion()
-
 for fileName in workingFiles:
     data = pd.read_csv(fileName, index_col=None)
     data.loc[:, 'Date'] = pd.to_datetime(data.loc[:, 'Date'])
     # Calculate the elapsed seconds to plot rather than using dates
     initialTime = data.loc[0, 'Date']
     elapsedTime = data.loc[:, 'Date'] - initialTime
-    data.loc[:, 'ElapsedTime(s)'] = elapsedTime.dt.total_seconds()/60.0/60.0
+    data.loc[:, 'ElapsedTime(s)'] = elapsedTime.dt.total_seconds()
+    markers = iT.cycle(['.', 'o', 's', '^'])  # Markers for diff. data types
     for dataType in data.loc[:, 'Type'].unique():
+        typeMark = next(markers)
         subData = data.loc[data.loc[:, 'Type'] == dataType, :]
-        ax1.errorbar(data.loc[:, 'ElapsedTime(s)'], data.loc[:, 'Avg'],
-                     yerr=data.loc[:, 'StdDev'], ls='none', marker='.',
-                     label=dataType)
-    plt.legend()
-
-sns.despine()
+        ax1.errorbar(data.loc[:, 'ElapsedTime(s)']/60.0/60.0,
+                     data.loc[:, 'Avg']*1000, yerr=data.loc[:, 'StdDev'], ls='none',
+                     marker=typeMark, label=dataType)
+        ax1.set_xlabel("Time Elapsed (hours)")
+        ax2.errorbar(data.loc[:, 'ElapsedTime(s)'],
+                     data.loc[:, 'Avg']*1000, yerr=data.loc[:, 'StdDev'], ls='none',
+                     marker=typeMark, label=dataType)
+        ax2.set_xlim([-100, 1.5*60*60.0])
+    ax1.legend()
+    ax2.legend()
+    data.to_csv(fileName[:-4]+" Result.csv", index_label="Date/Time")
+sns.despine(fig=f1)
+sns.despine(fig=f2)
+f1.savefig('WholeTime.svg', fmt='svg',dpi=1000)
+f2.savefig('90min.svg', fmt='svg',dpi=1000)
 plt.show()
