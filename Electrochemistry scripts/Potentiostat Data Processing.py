a7f563b03bfa3c6e5b485d9d40f720363811b619
machen <machen27@gmail.com>
2018-03-27 18:53:24 -0400

    Basic script functionality
    Still need to adjust data conversion to handle the dates better. Some of them are UNIX time stamps and some of them are elapsed times

--- Electrochemistry scripts/Potentiostat Data Processing.py
+++ Electrochemistry scripts/Potentiostat Data Processing.py
@@ -1,26 +1,30 @@
 import pandas as pd
-import numpy as np
+import datetime as dt
 import re
 
 dataPath = "R4_CurrentData.dat"
 
 expParamRE = re.compile('(date,)(.)*')
-expDataRE = re.compile('(Start run...)?(.)*')
+expDataRE = re.compile('(Start run...){1}(.)*')
 
 isExpParam = False
 isExpData = False
 expNo = 0  # Index for the experiments
+dataIndex = 0
 
 # Initialize data storage
 
 expParamColumns = ['date', 'Exp name', 'Voltage (mV)', 'dac_gain value',
                    'actual dac gain', 'adc_gain', 'adc_gain_trim',
                    'ExpLength (s)', 'AveragingTime (s)', 'refersh_freq']
-dataColumns = ['datetime', 'Current (A)', 'StdDev (A)', 'NPoints', 'expNo']
+dataColumns = ['Time from start (s)', 'Current (A)', 'StdDev (A)', 'NPoints',
+               'expNo']
 
 expParams = pd.DataFrame([], columns=expParamColumns)
 expParams.index.name = 'Exp No'
+
 expData = pd.DataFrame([], columns=dataColumns)
+expData.index.name = 'Measurement No'
 
 with open(dataPath, 'r') as file:
     for line in file:
@@ -28,25 +32,48 @@ with open(dataPath, 'r') as file:
         if isExpParam:
             # Casts the various input strings as the correct types
             params = line.split(',')
+            try:
+                # Need to convert Unix dates to regular dates
+                params[0] = dt.datetime.fromtimestamp(float(params[0]))\
+                                          .strftime('%Y-%m-%d %H:%M:%S')
+            except ValueError:
+                pass
             date = params[0]
             expName = params[1]
             voltage = float(params[2])
             dacGain = float(params[3])
             actualDacGain = float(params[4])
             adcGain = float(params[5])
-            expLength = float(params[6])
-            avgTime = float(params[7])
-            refreshFreq = float(params[8])
+            adcTrim = float(params[6])
+            expLength = float(params[7])
+            avgTime = float(params[8])
+            refreshFreq = float(params[9])
             # Write exp params to data table
-            expParams[expNo, :] = [date, expName, voltage, dacGain,
-                                   actualDacGain, adcGain, expLength, avgTime,
-                                   refreshFreq]
+            expParams.loc[expNo, :] = [date, expName, voltage, dacGain,
+                                       actualDacGain, adcGain, adcTrim,
+                                       expLength, avgTime, refreshFreq]
             expNo += 1
             isExpParam = False
-
+        if isExpData:
+            if line in ['\n', '\r\n']:
+                isExpData = False
+                continue
+            else:
+                data = line.strip('[]\n')
+                data = data.split(',')
+                pos = 0
+                for item in data:
+                    # Need to convert elapsed times to dates, and unix time stamps
+                    posLabel = dataColumns[pos]
+                    expData.loc[dataIndex, posLabel] = float(item)
+                    pos += 1
+                expData.loc[dataIndex, 'expNo'] = expNo
+                dataIndex += 1
         if expParamRE.match(line):
             isExpParam = True
             continue
         if expDataRE.match(line):
             isExpData = True
             continue
+expData.to_csv('Experimental Data.csv')
+expParams.to_csv('Experiment Parameters.csv')
